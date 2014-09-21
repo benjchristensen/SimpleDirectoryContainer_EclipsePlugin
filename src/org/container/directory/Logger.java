f8a6a5f5fcfdc2b1d60081b21b0cbff177c8986a
dpalic <darko.palic@xenovation.com>
2014-09-21 09:53:02 +0200

    fixed bug, that at startup the logger was not initialized. Not the plugin gets the logger from the Plugin Activator, which is started up by eclipse before accessing any other class in the plugin.

--- src/org/container/directory/Logger.java
+++ src/org/container/directory/Logger.java
@@ -2,46 +2,51 @@ package org.container.directory;
 
 import org.eclipse.core.runtime.ILog;
 import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.Status;
 
 /**
- * This is a simple logger that logs messages and stack traces to the Eclipse error log.
+ * This is a simple logger that logs messages and stack traces to the Eclipse
+ * error log.
  * 
  * @author Aaron J Tarter
  */
 public class Logger {
-    public final static String PLUGIN_ID = "org.container.directory";
-    // logging severities
-    public static final int OK = IStatus.OK;
-    public static final int ERROR = IStatus.ERROR;
-    public static final int CANCEL = IStatus.CANCEL;
-    public static final int INFO = IStatus.INFO;
-    public static final int WARNING = IStatus.WARNING;
-    // reference to the Eclipse error log
-    private static ILog log;
-    
-    /*
-     * Get a reference to the Eclipse error log
-     */
-    static {
-        log = Platform.getLog(Platform.getBundle(PLUGIN_ID));    
-    }
-    
-    /*
-     * Prints stack trace to Eclipse error log 
-     */
-    public static void log(int severity, Throwable e) {
-        Status s = new Status(severity, PLUGIN_ID, IStatus.OK, e.getMessage(), e);
-        log.log(s);
-    }
-    
-    /*
-     * Prints a message to the Eclipse error log
-     */
-    public static void log(int severity, String msg) {
-        Status s = new Status(severity, PLUGIN_ID, IStatus.OK, msg, null);
-        log.log(s);
-    }
+   public final static String PLUGIN_ID = "org.container.directory";
+   // logging severities
+   public static final int OK = IStatus.OK;
+   public static final int ERROR = IStatus.ERROR;
+   public static final int CANCEL = IStatus.CANCEL;
+   public static final int INFO = IStatus.INFO;
+   public static final int WARNING = IStatus.WARNING;
+
+   // reference to the Eclipse error log
+   private static ILog log;
+
+   /**
+    * Use the Activator, to get the logger.
+    * This ensures, that the logger will never be null.
+    */
+   private static ILog getLog() {
+      if (log == null) {
+         log = Activator.getDefault().getLog();
+      }
+      return log;
+   }
+
+   /*
+    * Prints stack trace to Eclipse error log
+    */
+   public static void log(int severity, Throwable e) {
+      Status s = new Status(severity, PLUGIN_ID, IStatus.OK, e.getMessage(), e);
+      getLog().log(s);
+   }
+
+   /*
+    * Prints a message to the Eclipse error log
+    */
+   public static void log(int severity, String msg) {
+      Status s = new Status(severity, PLUGIN_ID, IStatus.OK, msg, null);
+      getLog().log(s);
+   }
 
 }
