2a9e6c48df514d6bfef529b84dc1ea7d2ff96d6a
dpalic <darko.palic@xenovation.com>
2014-09-30 12:40:01 +0200

    fixed issue there this class MUST be immutable on getting the classpathentries. Now a update is requested on detecting a change

--- src/org/container/directory/SimpleDirContainer.java
+++ src/org/container/directory/SimpleDirContainer.java
@@ -4,11 +4,18 @@ import java.io.File;
 import java.io.FilenameFilter;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
+import org.eclipse.jdt.core.ClasspathContainerInitializer;
 import org.eclipse.jdt.core.IClasspathAttribute;
 import org.eclipse.jdt.core.IClasspathContainer;
 import org.eclipse.jdt.core.IClasspathEntry;
@@ -44,6 +51,19 @@ public class SimpleDirContainer implements IClasspathContainer {
    // Filename extensions to include in container
    private HashSet<String> _exts;
 
+   private IClasspathEntry[] classpathEntries;
+
+   private Status currentStatus = Status.Initializing;
+
+   /**
+    * Define the current status of this instance.
+    * 
+    * @author Darko Palic
+    */
+   private enum Status {
+      Initializing, InUse, RequestedContainerUpdate
+   }
+
    /**
     * This filename filter will be used to determine which files will be
     * included in the container
@@ -73,10 +93,13 @@ public class SimpleDirContainer implements IClasspathContainer {
          if (prefix.endsWith("-src")) {
             return false;
          }
-         // Darko TODO: handling of dir container extension must be improved at all. Just a hack for the previous code
+         // Darko TODO: handling of dir container extension must be improved at
+         // all. Just a hack for the previous code
          if (_exts == null) {
-            Logger.log(Logger.WARNING, "Directory Container could not read the extensions from .classpath for: " + dir.toString()
-                  + " of name " + name);
+            // Logger.log(Logger.WARNING,
+            // "Directory Container could not read the extensions from .classpath for: "
+            // + dir.toString()
+            // + " of name " + name);
             return true;
          } else {
             if (_exts.contains(ext)) {
@@ -98,14 +121,14 @@ public class SimpleDirContainer implements IClasspathContainer {
     * - comma separated list of extensions to include in this container
     * (extensions do not include the preceding ".")
     * 
-    * @param path
+    * @param containerPath
     *           unique path for this container instance, including directory and
     *           extensions a segments
     * @param project
     *           the Java project that is referencing this container
     */
-   public SimpleDirContainer(final IPath path, IJavaProject project) {
-      _path = path;
+   public SimpleDirContainer(final IPath containerPath, IJavaProject project) {
+      _path = containerPath;
       _project = project;
 
       // // extract the extension types for this container from the path
@@ -116,26 +139,42 @@ public class SimpleDirContainer implements IClasspathContainer {
       // _exts.add(ext.toLowerCase());
       // }
 
-      // extract the directory string from the PATH and create the directory
-      // relative
-      // to the project
-      IPath libsPath = createDescriptionPath(path, project);
+      File classPathFolder = getClasspathDirectory(containerPath, project);
+      _dir = classPathFolder;
+
+      // Create UI String for this container that reflects the directory being
+      // used
+      _desc = createDescription(containerPath, project);
+
+      classpathEntries = resolveLibsFromDir(_dir);
+
+      currentStatus = Status.InUse;
+   }
+
+   /**
+    * extract the directory string from the PATH and create the directory
+    * relative to the project.
+    * 
+    * @param containerPath
+    *           the full path of the container
+    * @param project
+    *           the container project
+    * @return the file reference to the dir
+    */
+   private File getClasspathDirectory(final IPath containerPath, IJavaProject project) {
+      IPath libsPath = createDescriptionPath(containerPath, project);
       File rootProj = project.getProject().getLocation().makeAbsolute().toFile();
       // remove indicator path of project
       IPath relativePath = libsPath.removeFirstSegments(1);
       File classPathFolder = new File(rootProj, relativePath.toString());
       if (!classPathFolder.exists()) {
-         Logger.log(Logger.WARNING, "Folder of Directory Container missing: " + libsPath.toString() + " of project " + project.getElementName()
-               + " creating it now to avoid errors in eclipse");
+         Logger.log(Logger.WARNING,
+               "Folder of Directory Container missing: " + libsPath.toString() + " of project " + project.getElementName()
+                     + " creating it now to avoid errors in eclipse");
 
          classPathFolder.mkdirs();
       }
-      _dir = classPathFolder;
-
-      // Create UI String for this container that reflects the directory being
-      // used
-      _desc = createDescription(path, project);
-
+      return classPathFolder;
    }
 
    /**
@@ -161,12 +200,38 @@ public class SimpleDirContainer implements IClasspathContainer {
     * @see org.eclipse.jdt.core.IClasspathContainer#getClasspathEntries()
     */
    public IClasspathEntry[] getClasspathEntries() {
+      if (isClasspathChanged() && (currentStatus != Status.RequestedContainerUpdate)) {
+         try {
+            // okay fine, we have an change in the classpath, so request an update to eclipse
+            IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+            IProject project = root.getProject(_project.getElementName());
+            IJavaProject javaProject = JavaCore.create(project);
+
+            // a very nice info, which is completly missing in the eclipse docs.
+            // http://stackoverflow.com/questions/12806888/refreshing-classpath-container-name-in-eclipse
+            // means this class may never change, so we will update the container and
+            // set a new one.
+            // The container must be immutable, so we have to request a update
+            ClasspathContainerInitializer initializer = JavaCore.getClasspathContainerInitializer(ID.toString());
+            initializer.requestClasspathContainerUpdate(_path, _project, new SimpleDirContainer(_path, javaProject));
+
+            currentStatus = Status.RequestedContainerUpdate;
+         } catch (CoreException e) {
+            Logger.log(Logger.ERROR, "getClasspathEntries - ERROR: " + e.getMessage());
+         }
+      }
+      
+      return classpathEntries;
+   }
+
+   private IClasspathEntry[] resolveLibsFromDir(File classpathDirectory) {
       ArrayList<IClasspathEntry> entryList = new ArrayList<IClasspathEntry>();
       // fetch the names of all files that match our filter
 
-      if (_dir.isDirectory()) {
+      File resolvedDir = classpathDirectory;
+      if (resolvedDir.isDirectory()) {
 
-         File[] libs = _dir.listFiles(_dirFilter);
+         File[] libs = resolvedDir.listFiles(_dirFilter);
 
          try {
             for (File lib : libs) {
@@ -197,6 +262,25 @@ public class SimpleDirContainer implements IClasspathContainer {
       IClasspathEntry[] entryArray = new IClasspathEntry[entryList.size()];
 
       return (IClasspathEntry[]) entryList.toArray(entryArray);
+   }
+
+   private boolean isClasspathChanged() {
+      boolean result = false;
+      
+      List<IClasspathEntry> current = Arrays.asList(classpathEntries);
+      IClasspathEntry[] newlyResolved = resolveLibsFromDir(_dir);
+      List<IClasspathEntry> difference = new ArrayList<IClasspathEntry>();
+      difference.addAll(current);
+      difference.removeAll(Arrays.asList(newlyResolved));
+            
+      if (newlyResolved.length != classpathEntries.length) {
+         // okay trivial test
+         result = true;
+      } else if (difference.size() > 0) {
+         // maybe same mount of references reside, but the libs have changed (e.g. filename, version, ...)
+         result = true;
+      }
+      return result;
    }
 
    /*
@@ -236,12 +320,12 @@ public class SimpleDirContainer implements IClasspathContainer {
    /*
     * @return whether or not this container would include the file
     */
-   public boolean isContained(File file) {
+   public boolean isFileExtensionContained(File file) {
       if (file.getParentFile().equals(_dir)) {
          // peel off file extension
          String fExt = file.toString().substring(file.toString().lastIndexOf('.') + 1);
          // check is it is in the set of cofigured extensions
-         if (_exts.contains(fExt.toLowerCase())) {
+         if ((_exts != null) && (_exts.contains(fExt.toLowerCase()))) {
             return true;
          }
       }
